1bf35b60b699d7798ddc25e35ba46b2ebd9ec628
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-12-28 21:25:08 +0100

    Add files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
-"use client"
-import { useState } from 'react';
-import LogoImage from '../assets/icons/logo.svg';
-import MenuIcon from '../assets/icons/menu.svg';
-import CloseIcon from '../assets/icons/close.svg';
+"use client";
+import { useState } from "react";
+import LogoImage from "../assets/icons/logo.svg";
+import MenuIcon from "../assets/icons/menu.svg";
+import CloseIcon from "../assets/icons/close.svg";
+import { useRouter } from "next/navigation";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const router = useRouter();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navigateToRide = () => {
+    router.push("/ride");
   };
 
   return (
@@ -27,13 +33,17 @@ export const Navbar = () => {
               className="border border-black border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg sm:hidden cursor-pointer"
               onClick={toggleMenu}
             >
-              {isMenuOpen ? <CloseIcon className="text-black" /> : <MenuIcon className="text-black" />}
+              {isMenuOpen ? (
+                <CloseIcon className="text-black" />
+              ) : (
+                <MenuIcon className="text-black" />
+              )}
             </div>
 
             {/* Navigation Links */}
             <nav
               className={`${
-                isMenuOpen ? 'flex' : 'hidden'
+                isMenuOpen ? "flex" : "hidden"
               } sm:flex flex-col sm:flex-row gap-6 items-center text-black absolute sm:static top-16 left-0 w-full sm:w-auto bg-white sm:bg-transparent z-10 sm:z-auto p-4 sm:p-0`}
             >
               <a
@@ -66,7 +76,10 @@ export const Navbar = () => {
               >
                 Customers
               </a>
-              <button className="bg-black py-2 px-4 rounded-lg text-white">
+              <button
+                className="bg-black py-2 px-4 rounded-lg text-white"
+                onClick={navigateToRide}
+              >
                 Ride for free
               </button>
             </nav>
