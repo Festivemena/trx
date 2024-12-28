96af4f20f00f92f7290aa04dafd15ccfded621c6
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-12-29 00:11:14 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tranxity - Bike hailing Service",
-  description: "Template for saas applications with dark theme",
+  description: "Rewarding Bike Hailing Service to aid Mobility",
 };
 
 export default function RootLayout({
